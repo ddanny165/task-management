738e92ac0452705cced557779d8195758c340c1b
Danylo Moskaliuk <danylo.moskaliuk@student.uibk.ac.at>
2024-09-15 23:43:11 +0200

    Added the project skeleton

---
+++ src/test/java/dev/ddanny165/taskManagement/MainTests.java
@@ -0,0 +1,13 @@
+package dev.ddanny165.taskManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
